f740df0215791d7b39dcb5b50d32ac5aae5ce117
IdeaPad <malikova418@gmail.com>
2024-04-01 13:09:49 +0600

    ///

--- src/Main.java
+++ src/Main.java
@@ -9,103 +9,123 @@ public class Main {
     public static void main(String[] args) {
         PersonService personService = new PersonServiceImpl();
         boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
         while (!exit) {
             System.out.println("""
-                    1.savePerson()
-                    2.getAllPerson()
-                    3.getPersonByName()
-                    4.updatePersonById()
-                    5.deletePersonById()
-                    6.exit
-                    """);
-            switch (new Scanner(System.in).nextLine()){
+                1.savePerson()
+                2.getAllPerson()
+                3.getPersonByName()
+                4.updatePersonById()
+                5.deletePersonById()
+                6.exit
+                """);
+            switch (scanner.nextLine()) {
                 case "1": {
                     Person person = new Person();
                     System.out.println("имя: ");
-                    person.setFirstName(new Scanner(System.in).nextLine());
+                    person.setFirstName(scanner.nextLine());
                     System.out.println("фамилия: ");
-                    person.setLastName(new Scanner(System.in).nextLine());
+                    person.setLastName(scanner.nextLine());
                     System.out.println("email: ");
-                    person.setEmail(new Scanner(System.in).nextLine());
+                    String email = scanner.nextLine();
                     boolean isTrue = false;
                     while (!isTrue) {
                         try {
-                            if (person.getEmail().contains("@"));
-                            isTrue = true;
+                            if (email.contains("@")) {
+                                isTrue = true;
+                            } else {
+                                throw new IllegalArgumentException("должен содержать @ ");
+                            }
                         } catch (IllegalArgumentException e) {
-                            System.err.println("должен содержать @ ");
+                            System.err.println(e.getMessage());
+                            email = scanner.nextLine();
                         }
                     }
+                    person.setEmail(email);
                     System.out.println("age: ");
+                    int age = 0;
                     boolean isTrue1 = false;
                     while (!isTrue1) {
                         try {
-                            if (person.getAge()>0 && person.getAge()<100);
-                            isTrue1 = true;
+                            age = Integer.parseInt(scanner.nextLine());
+                            if (age > 0 && age < 100) {
+                                isTrue1 = true;
+                            } else {
+                                throw new IllegalArgumentException("неверное значение");
+                            }
                         } catch (IllegalArgumentException e) {
-                            System.err.println("неверное значение");
+                            System.err.println(e.getMessage());
                         }
                     }
-                    person.setAge(new Scanner(System.in).nextInt());
+                    person.setAge(age);
                     person.setId(GeneratedId.genPersonId());
-                    System.out.println("личный ID: "+person.getId());
+                    System.out.println("личный ID: " + person.getId());
                     personService.savePerson(person);
                     break;
                 }
-                case "2":{
+                case "2": {
                     System.out.println(personService.getAllPerson());
                     break;
                 }
-                case "3":{
+                case "3": {
                     System.out.println("Введите имя: ");
-                    System.out.println(personService.getPersonByName(new Scanner(System.in).nextLine()));
+                    System.out.println(personService.getPersonByName(scanner.nextLine()));
                     break;
                 }
-                case "4":{
+                case "4": {
                     System.out.println("Введите id: ");
                     personService.updatePersonById(new Scanner(System.in).nextLong());
                     break;
                 }
-                case "5":{
+                case "5": {
                     Person person = new Person();
                     System.out.println("новое имя: ");
-                    person.setFirstName(new Scanner(System.in).nextLine());
-                    System.out.println("новую фамилия: ");
-                    person.setLastName(new Scanner(System.in).nextLine());
+                    person.setFirstName(scanner.nextLine());
+                    System.out.println("новая фамилия: ");
+                    person.setLastName(scanner.nextLine());
                     System.out.println("новый email: ");
-                    person.setEmail(new Scanner(System.in).nextLine());
+                    String email = scanner.nextLine();
                     boolean isTrue = false;
                     while (!isTrue) {
                         try {
-                            if (person.getEmail().contains("@"));
-                            isTrue = true;
+                            if (email.contains("@")) {
+                                isTrue = true;
+                            } else {
+                                throw new IllegalArgumentException("должен содержать @ ");
+                            }
                         } catch (IllegalArgumentException e) {
-                            System.err.println("должен содержать @ ");
+                            System.err.println(e.getMessage());
+                            email = scanner.nextLine();
                         }
                     }
+                    person.setEmail(email);
                     System.out.println("age: ");
-                    System.out.println("age: ");
+                    int age = 0;
                     boolean isTrue1 = false;
                     while (!isTrue1) {
                         try {
-                            if (person.getAge()>0 && person.getAge()<100);
-                            isTrue1 = true;
+                            age = Integer.parseInt(scanner.nextLine());
+                            if (age > 0 && age < 100) {
+                                isTrue1 = true;
+                            } else {
+                                throw new IllegalArgumentException("неверное значение");
+                            }
                         } catch (IllegalArgumentException e) {
-                            System.err.println("неверное значение");
+                            System.err.println(e.getMessage());
                         }
                     }
-                    person.setAge(new Scanner(System.in).nextInt());
-                    person.setId(GeneratedId.genPersonId());
-
+                    person.setAge(age);
                     System.out.println("введите Id: ");
                     System.out.println(personService.deletePersonById(new Scanner(System.in).nextLong()));
                     break;
                 }
-                case "6":{
+                case "6": {
                     exit = true;
                     break;
                 }
             }
         }
+
     }
+
 }
